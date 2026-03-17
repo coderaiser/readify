e7ae40eed79d5263da45aafaf21d04e6d8fcafd8
coderiaser <coderaiser@cloudcmd.io>
2026-03-17 20:37:40 +0200

    feature: readdir: get rid of mock-require

--- test/readdir.js
+++ test/readdir.js
@@ -1,44 +1,31 @@
 'use strict';
 
-const fs = require('node:fs');
-
 const {test, stub} = require('supertape');
-const mockRequire = require('mock-require');
 
 const {tryToCatch} = require('try-to-catch');
-const {reRequire, stopAll} = mockRequire;
+const {readdir: _readdir} = require('../lib/readdir');
+
 const noop = () => {};
 
 test('readdir: empty dir', async (t) => {
-    const {readdir} = fs.promises;
+    const readdir = stub().resolves([]);
     
-    fs.promises.readdir = async () => [];
-    
-    const _readdir = reRequire('../lib/readdir');
-    const [, result] = await tryToCatch(_readdir, '.');
-    
-    fs.promises.readdir = readdir;
+    const [, result] = await tryToCatch(_readdir, '.', {
+        readdir,
+    });
     
     t.deepEqual(result, [], 'should return empty array');
     t.end();
 });
 
 test('readdir: empty stat', async (t) => {
-    const {readdir} = fs.promises;
+    const superstat = stub().rejects(Error('some'));
+    const readdir = stub().resolves(['hello']);
     
-    mockRequire('superstat', async () => {
-        throw Error('some');
+    const [, result] = await tryToCatch(_readdir, '/', {
+        readdir,
+        superstat,
     });
-    
-    fs.promises.readdir = async () => [
-        'hello',
-    ];
-    
-    const _readdir = reRequire('../lib/readdir');
-    
-    const [, result] = await tryToCatch(_readdir, '/');
-    
-    fs.promises.readdir = readdir;
     
     const expected = [{
         name: 'hello',
@@ -49,26 +36,22 @@ test('readdir: empty stat', async (t) => {
         type: 'file',
     }];
     
-    stopAll();
-    
     t.deepEqual(result, expected, 'should return empty array');
     t.end();
 });
 
 test('readdir: result', async (t) => {
-    const {readdir} = fs.promises;
-    
     const name = 'hello.txt';
     const mode = 16_893;
     const size = 1024;
     const mtime = new Date();
     const uid = 1000;
     
-    fs.promises.readdir = async () => [
+    const readdir = stub().resolves([
         name,
-    ];
+    ]);
     
-    mockRequire('superstat', async () => ({
+    const superstat = stub().resolves({
         isDirectory: noop,
         isSymbolicLink: noop,
         name,
@@ -76,7 +59,7 @@ test('readdir: result', async (t) => {
         size,
         mtime,
         uid,
-    }));
+    });
     
     const expected = [{
         name,
@@ -87,30 +70,27 @@ test('readdir: result', async (t) => {
         type: 'file',
     }];
     
-    const _readdir = reRequire('../lib/readdir');
-    const [, result] = await tryToCatch(_readdir, '.');
-    
-    fs.promises.readdir = readdir;
-    stopAll();
+    const [, result] = await tryToCatch(_readdir, '.', {
+        readdir,
+        superstat,
+    });
     
     t.deepEqual(result, expected, 'should get raw values');
     t.end();
 });
 
 test('readdir: result: no error', async (t) => {
-    const {readdir} = fs.promises;
-    
     const name = 'hello.txt';
     const mode = 16_893;
     const size = 1024;
     const mtime = new Date();
     const uid = 1000;
     
-    fs.promises.readdir = () => [
+    const readdir = stub().resolves([
         name,
-    ];
+    ]);
     
-    mockRequire('superstat', async () => ({
+    const superstat = stub().resolves({
         isDirectory: noop,
         isSymbolicLink: noop,
         name,
@@ -118,30 +98,27 @@ test('readdir: result: no error', async (t) => {
         size,
         mtime,
         uid,
-    }));
+    });
     
-    const _readdir = reRequire('../lib/readdir');
-    const [e] = await tryToCatch(_readdir, '.');
-    
-    stopAll();
-    fs.promises.readdir = readdir;
+    const [e] = await tryToCatch(_readdir, '.', {
+        readdir,
+        superstat,
+    });
     
     t.notOk(e, e?.message || 'should not receive error');
     t.end();
 });
 
 test('readdir: result: directory link', async (t) => {
-    const {readdir} = fs.promises;
-    
     const name = 'hello';
     const mode = 16_893;
     const size = 1024;
     const mtime = new Date();
     const uid = 1000;
     
-    fs.promises.readdir = () => [
+    const readdir = stub().resolves([
         name,
-    ];
+    ]);
     
     const info = {
         isDirectory: stub().returns(true),
@@ -155,7 +132,7 @@ test('readdir: result: directory link', async (t) => {
         dev: 1337,
     };
     
-    mockRequire('superstat', async () => info);
+    const superstat = stub().resolves(info);
     
     const expected = [{
         name,
@@ -166,11 +143,10 @@ test('readdir: result: directory link', async (t) => {
         type: 'directory-link',
     }];
     
-    const _readdir = reRequire('../lib/readdir');
-    const [, result] = await tryToCatch(_readdir, '.');
-    
-    fs.promises.readdir = readdir;
-    stopAll();
+    const [, result] = await tryToCatch(_readdir, '.', {
+        readdir,
+        superstat,
+    });
     
     t.deepEqual(result, expected, 'should get raw values');
     t.end();
@@ -183,9 +159,9 @@ test('readdir: result: zip link', async (t) => {
     const mtime = new Date();
     const uid = 1000;
     
-    const readdir = () => [
+    const readdir = stub().resolves([
         name,
-    ];
+    ]);
     
     const info = {
         isDirectory: stub().returns(false),
@@ -199,10 +175,7 @@ test('readdir: result: zip link', async (t) => {
         dev: 1337,
     };
     
-    mockRequire('node:fs/promises', {
-        readdir,
-    });
-    mockRequire('superstat', async () => info);
+    const superstat = stub().resolves(info);
     
     const expected = [{
         name,
@@ -213,43 +186,40 @@ test('readdir: result: zip link', async (t) => {
         type: 'archive-link',
     }];
     
-    const _readdir = reRequire('../lib/readdir');
-    const [, result] = await tryToCatch(_readdir, '.');
-    
-    stopAll();
+    const [, result] = await tryToCatch(_readdir, '.', {
+        readdir,
+        superstat,
+    });
     
     t.deepEqual(result, expected, 'should get raw values');
     t.end();
 });
 
 test('readdir: result: directory link: no error', async (t) => {
-    const {readdir} = fs.promises;
-    
     const name = 'hello';
     const mode = 16_893;
     const size = 1024;
     const mtime = new Date();
     const uid = 1000;
     
-    fs.promises.readdir = () => [
+    const readdir = stub().resolves([
         name,
-    ];
+    ]);
     
-    mockRequire('superstat', async () => ({
-        isDirectory: stub.returns(true),
-        isSymbolicLink: stub.returns(true),
+    const superstat = stub().resolves({
+        isDirectory: stub().returns(true),
+        isSymbolicLink: stub().returns(true),
         name,
         mode,
         size,
         mtime,
         uid,
-    }));
+    });
     
-    const _readdir = reRequire('../lib/readdir');
-    const [e] = await tryToCatch(_readdir, '.');
-    
-    stopAll();
-    fs.promises.readdir = readdir;
+    const [e] = await tryToCatch(_readdir, '.', {
+        readdir,
+        superstat,
+    });
     
     t.notOk(e, e?.message || 'should not receive error');
     t.end();
